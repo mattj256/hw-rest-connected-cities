b71518d860242fe345cdab7563920a44898da8cb
Matthew Jacobs <5423966+mattj256@users.noreply.github.com>
2018-04-20 11:58:42 -0400

    Make skeleton of real API

--- src/main/java/com/dreamsmadevisible/connectedcities/ConnectedCitiesController.java
+++ src/main/java/com/dreamsmadevisible/connectedcities/ConnectedCitiesController.java
@@ -7,12 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConnectedCitiesController {
+  // TODO: move this into an external configuration file
+  private static final String RESPONSE_YES = "yes";
+  private static final String RESPONSE_NO = "no";
 
-  private static final String template = "Hello, %s!";
-  private final AtomicLong counter = new AtomicLong();
+  // private static final String template = "Hello, %s!";
+  // private final AtomicLong counter = new AtomicLong();
 
+  @RequestMapping("/connected")
+  public ConnectedCitiesResponse greeting(
+      @RequestParam(value="origin", defaultValue="") String origin,
+      @RequestParam(value="destination", defaultValue="") String destination) {
+    return new ConnectedCitiesResponse("yes");
+  }
+
+  /*
   @RequestMapping("/greeting2")
   public ConnectedCitiesResponse greeting(@RequestParam(value="name", defaultValue="World") String name) {
     return new ConnectedCitiesResponse(counter.incrementAndGet(), String.format(template, name));
   }
+  */
 }
--- src/main/java/com/dreamsmadevisible/connectedcities/ConnectedCitiesResponse.java
+++ src/main/java/com/dreamsmadevisible/connectedcities/ConnectedCitiesResponse.java
@@ -2,19 +2,21 @@ package com.dreamsmadevisible.connectedcities;
 
 public class ConnectedCitiesResponse {
 
-  private final long id;
-  private final String content;
+  // private final long id;
+  private final String response;
 
-  public ConnectedCitiesResponse(long id, String content) {
-    this.id = id;
-    this.content = content;
+  public ConnectedCitiesResponse(/* long id, */ String response) {
+    // this.id = id;
+    this.response = response;
   }
 
+  /*
   public long getId() {
     return id;
   }
+  */
 
-  public String getContent() {
-    return content;
+  public String getResponse() {
+    return response;
   }
 }
